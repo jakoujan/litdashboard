bc0c3c8bd2f00b85ea2ff8fce481581112d949e0
jakoujan <jakoujan@gmail.com>
2025-02-13 10:11:13 -0600

    merge all request

--- src/elements/header-element.ts
+++ src/elements/header-element.ts
@@ -10,6 +10,7 @@ export class HeaderElement extends LitElement {
         return html`
         <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
+            <side-bar></side-bar>
             <span class="navbar-brand mb-0 h1">MyApp</span>
         </div>
         </nav>
